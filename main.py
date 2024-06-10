94c3e03712e4baa106f20d2339e8dcf106d30e9c
Vyacheslav <slawushkin@mail.ru>
2024-06-10 20:26:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+name = 'Vyacheslav'
+print('name: ', (name))
+age = 48
+print('age: ', (age))
+age = (age +2)
+print('new age: ', (age))
+is_student = (10>5 and 5<10 and 5==5 or 5!=5)
+print('is_student: ', (is_student))
+
